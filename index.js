790dfcd8db3f1e06dbfe4782daac6a923bb9443e
ARAVIND KRISHNAN K <aravindkrishnankr@gmail.com>
2024-07-01 00:49:47 +0530

    added backspace function

--- index.js
+++ index.js
@@ -5,6 +5,13 @@ const display = document.getElementById("display");
 function appendToDisplay(input) {
     display.value += input;
 }
+function removeLastInput() {
+    const displayElement = document.getElementById('display');
+    const currentValue = displayElement.value;
+    displayElement.value = currentValue.substring(0, currentValue.length - 1);
+  }
+  
+
 
 function clearDisplay() {
     display.value = "";
